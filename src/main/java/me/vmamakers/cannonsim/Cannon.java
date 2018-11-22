03fb303c929326809b79b298e9ec223a9a918cb3
Benjamin Zalla <bzalla10@gmail.com>
2018-11-22 15:48:29 -0500

    Basic class info

--- src/main/java/me/vmamakers/cannonsim/Cannon.java
+++ src/main/java/me/vmamakers/cannonsim/Cannon.java
@@ -2,8 +2,8 @@ package me.vmamakers.cannonsim;
 
 public class Cannon {
 	
-	private float angle;
-	private float length;
-	private float explodingForce;
+	private float angle; //angle of elevation of cannon with respect to horizontal
+	private float length; //length of cannon
+	private float avgExplodingForce; //average exploding force within combustion chamber of cannon; used to find v0 using work-energy theorem
 	
 }
--- src/main/java/me/vmamakers/cannonsim/CannonBall.java
+++ src/main/java/me/vmamakers/cannonsim/CannonBall.java
@@ -1,5 +1,9 @@
 package me.vmamakers.cannonsim;
 
 public class CannonBall {
-
+	
+	private float m; //mass of ball
+	private float r; //radius of ball; used for drag calculation
+	private float cD; //spherical drag coefficient
+	
 }
